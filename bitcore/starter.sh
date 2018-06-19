15e82821b04e1f7a9ae3e8aff857433150e46069
David B <37987466+dalijolijo@users.noreply.github.com>
2018-06-19 20:32:04 +0200

    Changes for BTX

--- bitcore/starter.sh
+++ bitcore/starter.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sed -i -- "s/\"livenet\"/\"${BITCOIN_NETWORK}\"/g" /root/config.json
-/root/node_modules/.bin/bitcore start -c /root/config.json
+sed -i -- "s/\"livenet\"/\"${BITCORE_NETWORK}\"/g" /root/config.json
+/root/node_modules/.bin/btxcore start -c /root/config.json
